10ff52bcce50074e94ee702e184d7d0f55d323f9
Brandon <brandon.michael.burt@gmail.com>
2024-02-28 10:35:17 -0500

    Resolved mobile styling issues for rendering

--- src/App.tsx
+++ src/App.tsx
@@ -29,7 +29,7 @@ export default function App() {
     }, []);
 
     return (
-        <div className='content'>
+        <div>
             {data && !error ? (
                     <>
                         <TokenPairInfo {...generatePairInfo(data)} />
--- src/index.tsx
+++ src/index.tsx
@@ -3,4 +3,8 @@ import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-root.render(<App />);
+root.render(
+    <div className='app'>
+        <App />
+    </div>
+);
